637fb515aeeb20c4ffcdd6ae60843ae8ef333b7a
closer <closer009@gmail.com>
2018-04-17 19:37:51 +0900

    functional option pattern

--- cryptor.go
+++ cryptor.go
@@ -13,14 +13,6 @@ type Decryptor interface {
 	Decrypt(interface{}) (interface{}, error)
 }
 
-type Config struct {
-	SimpleConfig
-	AwsConfig
-	GcpConfig
-
-	Cryptor string
-}
-
 func NewCryptor(config *Config) Cryptor {
 	var cryptor Cryptor
 	switch config.Cryptor {
@@ -35,4 +27,45 @@ func NewCryptor(config *Config) Cryptor {
 	}
 
 	return cryptor
+}
+
+type Config struct {
+	SimpleConfig
+	AwsConfig
+	GcpConfig
+
+	Cryptor string
+}
+
+type Option func(c *Config) error
+
+func NewConfig(cryptor string, opts ...Option) *Config {
+	config := &Config{
+		Cryptor: cryptor,
+	}
+	for _, opt := range opts {
+		opt(config)
+	}
+	return config
+}
+
+func WithSimpleConfig(sc *SimpleConfig) Option {
+	return func(c *Config) error {
+		c.SimpleConfig = *sc
+		return nil
+	}
+}
+
+func WithAwsConfig(ac *AwsConfig) Option {
+	return func(c *Config) error {
+		c.AwsConfig = *ac
+		return nil
+	}
+}
+
+func WithGcpConfig(gc *GcpConfig) Option {
+	return func(c *Config) error {
+		c.GcpConfig = *gc
+		return nil
+	}
 }
